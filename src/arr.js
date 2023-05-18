1c47aeb66a4415da42d111799301085551ea8928
sholom perman <sholom@kali.MyLapTop>
2023-05-17 22:37:39 -0400

    added teny

--- src/arr.js
+++ src/arr.js
@@ -6,8 +6,10 @@ import snapchat from "./icons/snapchat.svg";
 import github from "./icons/github.svg";
 import phone from "./icons/phone.svg";
 import mail from "./icons/mail.svg";
-import contact from "./assets/Sholom-Perman.vcf";
+import sholom_vcs from "./assets/Sholom-Perman.vcf";
+import Mendel_vcs from "./assets/Mendel-ten.vcf";
 import sholomperman from "./assets/sholom-profile.jpeg";
+import teny from "./assets/teny.jpeg";
 
 export const arr = [
   {
@@ -16,7 +18,7 @@ export const arr = [
     route: '/sholomperman',
     avatar: sholomperman,
     bio: "Full Stack Developer",
-    contact: contact,
+    contact: sholom_vcs,
     hashtags:
       "HTML CSS JavaScript reactJS nodeJS (sass tailwind materialUI bootstrap)",
     links: [
@@ -37,7 +39,7 @@ export const arr = [
       },
       {
         id: 4,
-        url: "https://twitter.com/sholomperman",
+        url: "https://twitter.com/MendelTenenbaum",
         icon: twitter,
       },
       {
@@ -62,60 +64,61 @@ export const arr = [
       },
     ],
   },
-  // {
-  //   id: 2,
-  //   name: "test",
-  //   avatar: sholomperman,
-  //   bio: "test",
-  //   contact: contact,
-  //   hashtags:
-  //     "The boi",
-  //   links: [
-  //     {
-  //       id: 1,
-  //       url: "https://instagram.com/sholomperman",
-  //       icon: instagram,
-  //     },
-  //     {
-  //       id: 2,
-  //       url: "https://linkedin.com/in/sholom-perman",
-  //       icon: linkedin,
-  //     },
-  //     {
-  //       id: 3,
-  //       url: "https://www.facebook.com/profile.php?id=100072370041022",
-  //       icon: facebook,
-  //     },
-  //     {
-  //       id: 4,
-  //       url: "https://twitter.com/sholomperman",
-  //       icon: twitter,
-  //     },
-  //     {
-  //       id: 5,
-  //       url: "https://github.com/sholomperman",
-  //       icon: github,
-  //     },
-  //     {
-  //       id: 6,
-  //       url: "https://www.snapchat.com/add/sholomperman?share_id=vwOHV8Nv3HE&locale=en-US",
-  //       icon: snapchat,
-  //     },
-  //     {
-  //       id: 7,
-  //       url: "tel:3473789160",
-  //       icon: phone,
-  //     },
-  //     {
-  //       id: 8,
-  //       url: "mailto:sholomperman",
-  //       icon: mail,
-  //     },
-  //     {
-  //       id: 8,
-  //       url: pdf,
-  //       icon: resume,
-  //     },
-  //   ],
-  // },
+  {
+    id: 2,
+    name: "Mendel Tenenbaum",
+    route: "/mendeltenenbaum",
+    avatar: teny,
+    bio: "Independent contractor.",
+    contact: Mendel_vcs,
+    hashtags:
+      "blue collar operations manager, avid traveler, pilot in training.",
+    links: [
+      {
+        id: 1,
+        url: "https://www.instagram.com/the_10y/",
+        icon: instagram,
+      },
+      {
+        id: 2,
+        url: "https://linkedin.com/in/sholom-perman",
+        icon: linkedin,
+      },
+      {
+        id: 3,
+        url: "https://www.facebook.com/mendel.tenenbaum.9",
+        icon: facebook,
+      },
+      {
+        id: 4,
+        url: "https://twitter.com/sholomperman",
+        icon: twitter,
+      },
+      // {
+      //   id: 5,
+      //   url: "https://github.com/sholomperman",
+      //   icon: github,
+      // },
+      // {
+      //   id: 6,
+      //   url: "https://www.snapchat.com/add/sholomperman?share_id=vwOHV8Nv3HE&locale=en-US",
+      //   icon: snapchat,
+      // },
+      {
+        id: 7,
+        url: "tel:7076990731",
+        icon: phone,
+      },
+      {
+        id: 8,
+        url: "mailto:mendelten",
+        icon: mail,
+      },
+      // {
+      //   id: 8,
+      //   url: pdf,
+      //   icon: resume,
+      // },
+    ],
+  },
 ];
